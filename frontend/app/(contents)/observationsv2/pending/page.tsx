22684ac96107f8842fc23e189bc511936aca53f0
tylerastro <w39398898@gmail.com>
2024-05-12 15:57:56 +0800

    Fix: useSearchParams() should be wrapped in a suspense boundary

--- frontend/app/(contents)/observationsv2/pending/page.tsx
+++ frontend/app/(contents)/observationsv2/pending/page.tsx
@@ -55,24 +55,25 @@ export default function Page() {
 
   return (
     <>
-      <div className="flex space-between justify-between pb-10">
-        <div>
-          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-primary-foreground">
-            Verify the submission
-          </h1>
+      <Suspense fallback={LoadingSkeleton()}>
+        <div className="flex space-between justify-between pb-10">
+          <div>
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-primary-foreground">
+              Verify the submission
+            </h1>
+          </div>
+
+          <div className="flex gap-2">
+            <Button
+              onClick={handleVerify}
+              variant="outline"
+              className="dark:hover:bg-green-600"
+            >
+              Verify
+            </Button>
+          </div>
         </div>
 
-        <div className="flex gap-2">
-          <Button
-            onClick={handleVerify}
-            variant="outline"
-            className="dark:hover:bg-green-600"
-          >
-            Verify
-          </Button>
-        </div>
-      </div>
-      <Suspense fallback={LoadingSkeleton()}>
         <PageContent />
       </Suspense>
     </>
