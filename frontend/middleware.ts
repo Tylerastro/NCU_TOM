8a8dbc4f4638c412b8e624d3bd9e296e6ef6a6d2
tylerastro <w39398898@gmail.com>
2024-05-29 15:28:36 +0800

    Feat: add role based middleware

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,1 +1,19 @@
-export { auth as middleware } from "@/auth";
+import { auth } from "@/auth";
+import { UserRole } from "./models/enums";
+
+const protectedRoutes = ["/dashboard", "/dashboard/lulin", "/settings"];
+
+export default auth((req) => {
+  if (
+    protectedRoutes.some((route) => req.nextUrl.pathname.startsWith(route)) &&
+    !req.auth?.user &&
+    req.auth?.user.role !== UserRole.Admin
+  ) {
+    const homepage = process.env.NEXT_HOME_URL;
+    return Response.redirect(homepage as string);
+  }
+});
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
