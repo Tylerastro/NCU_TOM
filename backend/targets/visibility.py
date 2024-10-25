7b7d1460194d78ce9707c296db160dde22c67678
tylerastro <w39398898@gmail.com>
2024-10-25 17:10:47 +0800

    Feat: responsive time resolution on observation period

--- backend/targets/visibility.py
+++ backend/targets/visibility.py
@@ -43,6 +43,17 @@ class Visibility:
         self.time_resolution = time_resolution
         self.time_offset = time_offset
 
+    def _get_time_resolution(self, start_time: Time, end_time: Time):
+        time_diff = end_time - start_time
+        if time_diff < 2 * u.day:
+            return 15 * u.minute
+        elif time_diff < 3 * u.day:
+            return 25 * u.minute
+        elif time_diff < 4 * u.day:
+            return 35 * u.minute
+        else:
+            return 45 * u.minute
+
     def is_moon_interfering(self, target, observation_time) -> bool:
         moon = get_body('moon', observation_time,
                         location=self.observatory_location)
@@ -74,7 +85,8 @@ class Visibility:
 
     def get_altaz(self, name, ra, dec, start_time: Time, end_time: Time) -> TargetAltAz:
         target = SkyCoord(ra=ra*u.deg, dec=dec*u.deg, frame='icrs')
-        time_resolution = TimeDelta(self.time_resolution).to(u.day).value
+        time_resolution = TimeDelta(self._get_time_resolution(
+            start_time, end_time)).to(u.day).value
         jd_range = np.arange(start_time.jd, end_time.jd +
                              time_resolution, time_resolution)
         observation_times = Time(jd_range, format='jd', scale='utc')
