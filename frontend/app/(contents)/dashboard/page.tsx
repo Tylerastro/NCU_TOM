b6d8ae3e36b1c5b1069ab315f0594dd686a4116a
tylerastro <w39398898@gmail.com>
2024-11-02 16:05:13 +0800

    Fix: remove error in build Refactor: update observation api

--- frontend/app/(contents)/dashboard/page.tsx
+++ frontend/app/(contents)/dashboard/page.tsx
@@ -4,12 +4,9 @@ import { redirect } from "next/navigation";
 import DashboardPage from "./lulin/lulinDashboard";
 import AdminDashboard from "./users/adminDashboard";
 
-export default async function page() {
+export default async function Page() {
   const session = await auth();
-  if (
-    session?.user.role === UserRole.User ||
-    session?.user.role === UserRole.Visitor
-  ) {
+  if (session?.user.role === UserRole.User) {
     redirect("/");
   } else if (session?.user.role === UserRole.Admin) {
     return <AdminDashboard />;
